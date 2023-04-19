72651f8a03e719a8e7eed46c86c6fd783c955a1b
JulianSP02 <jsolis30315@ufide.ac.cr>
2023-04-19 17:58:27 -0600

    Timer y puerta para finalizar nivel

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField]
+    float timer = 121.0F;
+
+    [SerializeField]
+    Text textTimer;
+
+    Level3DManager levelManager;
+
+    void Awake()
+    {
+        levelManager = FindObjectOfType<Level3DManager>();
+
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        textTimer.text = "" + timer.ToString();
+
+        if(timer == 0.0F)
+        {
+            SceneManager.GetSceneByName("Game Over");
+        }
+    } 
+}
